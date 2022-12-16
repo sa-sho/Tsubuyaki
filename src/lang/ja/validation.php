ef2cd8d3276628c7d235794a9cd3461e91759c38
s-sho <sh-sato@nixe-inc.com>
2022-12-16 15:16:52 +0900

    エラーメッセージの追加、変更

--- src/lang/ja/validation.php
+++ src/lang/ja/validation.php
@@ -132,4 +132,7 @@ return [
     'uppercase'            => ':Attributeは、大文字で入力してください。',
     'url'                  => ':Attributeは、有効なURL形式で指定してください。',
     'uuid'                 => ':Attributeは、有効なUUIDでなければいけません。',
+    'attributes'           => [
+        'tweet'   => 'つぶやき'
+    ],
 ];
