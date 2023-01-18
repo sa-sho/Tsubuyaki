4781e9e6f53430439691b2f3e7d2ed0401d978a4
s-sho <sh-sato@nixe-inc.com>
2023-01-18 16:28:36 +0900

    renderで実験

--- src/scripts/00-laravel-deploy.sh
+++ src/scripts/00-laravel-deploy.sh
@@ -1,22 +1,45 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+# Change to the project directory
+cd $FORGE_SITE_PATH
 
-echo "Caching config..."
-php artisan config:cache
+# Turn on maintenance mode
+php artisan down || true
 
-echo "Caching routes..."
-php artisan route:cache
+# Pull the latest changes from the git repository
+# git reset --hard
+# git clean -df
+git pull origin $FORGE_SITE_BRANCH
 
-echo "Caching views..."
-php artisan view:cache
+# Install/update composer dependecies
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
-echo "Running migrations..."
+# Restart FPM
+( flock -w 10 9 || exit 1
+    echo 'Restarting FPM...'; sudo -S service $FORGE_PHP_FPM reload ) 9>/tmp/fpmlock
+
+# Run database migrations
 php artisan migrate --force
 
-echo "install node modules..."
+# Clear caches
+php artisan cache:clear
+
+# Clear expired password reset tokens
+php artisan auth:clear-resets
+
+# Clear and cache routes
+php artisan route:cache
+
+# Clear and cache config
+php artisan config:cache
+
+# Clear and cache views
+php artisan view:cache
+
+# Install node modules
 npm ci
 
-echo "Running build..."
+# Build assets using Laravel Mix
 npm run build
+
+# Turn off maintenance mode
+php artisan up
