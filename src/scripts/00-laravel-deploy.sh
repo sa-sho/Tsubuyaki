0d1abc7e3676abacba3c0ede810069d05b389777
s-sho <sh-sato@nixe-inc.com>
2023-01-18 16:30:56 +0900

    renderで実験

--- src/scripts/00-laravel-deploy.sh
+++ src/scripts/00-laravel-deploy.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 # Change to the project directory
-cd $FORGE_SITE_PATH
+cd $src
 
 # Turn on maintenance mode
 php artisan down || true
 
-# Pull the latest changes from the git repository
-# git reset --hard
-# git clean -df
-git pull origin $FORGE_SITE_BRANCH
-
 # Install/update composer dependecies
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
-
-# Restart FPM
-( flock -w 10 9 || exit 1
-    echo 'Restarting FPM...'; sudo -S service $FORGE_PHP_FPM reload ) 9>/tmp/fpmlock
 
 # Run database migrations
 php artisan migrate --force
